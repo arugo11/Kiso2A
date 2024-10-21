969e9b6c0d1763dd1b713920b40b8bdfd7b53316
arugo11 <al23088@shibaura-it.ac.jp>
2024-10-21 16:09:50 +0900

    add:入力処理実装

---
+++ src/lesson4/java0000/Test04A.java
@@ -0,0 +1,10 @@
+// import com.shibaura.tools.*;
+import com.shibaura.system.*;
+
+class Test04A {
+    public static void main(String[] args){
+        Car myCar = new Car();
+        myCar.setSpeed(100.0);
+        System.out.println("velocity" + myCar.getSpeed());
+    }
+}
