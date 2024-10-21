752dd1d0be5945c6204b1c089d18bedda8c5ab24
arugo11 <al23088@shibaura-it.ac.jp>
2024-10-21 16:43:18 +0900

    add:4.3ファイル入出力

--- src/lesson4/Test04B.java
+++ src/lesson4/Test04B.java
@@ -3,14 +3,27 @@ import java.io.*;
 class Test04B{
     public static void main(String[] args){
         try{
+            /*読み込み */
             FileInputStream fis = new FileInputStream("input.txt");
             InputStreamReader isr = new InputStreamReader(fis,"SJIS");
             BufferedReader br = new BufferedReader(isr);
             String line;
+
+            PrintWriter pw = new PrintWriter(
+                new BufferedWriter(
+                    new FileWriter("output.txt")
+                )
+            );
+
             while ((line = br.readLine()) != null){
                 System.out.println(line);
+                pw.println(line);
             }
+
+
             br.close();
+            pw.close();
+
         }catch(IOException e){
             e.printStackTrace();
         }
