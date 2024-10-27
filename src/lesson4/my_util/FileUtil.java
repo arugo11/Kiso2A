2887a9b8e86e4c9efb7a3c44cc5e784f834aeac2
arugo11 <al23088@shibaura-it.ac.jp>
2024-10-27 22:46:46 +0900

    ref:StringBuilderを利用

--- src/lesson4/my_util/FileUtil.java
+++ src/lesson4/my_util/FileUtil.java
@@ -1,19 +1,27 @@
+/**
+ * @file : FileUtil.java
+ */
+
 package my_util;
 
 import java.io.*;
 
 public class FileUtil {
+    /**
+     * ファイルを読み込む関数
+     * @param filename 読み込むファイル名
+     * @return 各行の末尾に改行文字を加えた文字列
+     */
     public static String readTextFile(String filename){
         try{
             String line;
-            String text = "";
               BufferedReader br =
                 new BufferedReader(
                     new FileReader(filename));
+            StringBuilder text = new StringBuilder();
             while((line = br.readLine()) != null){
-                text += line + "\n";
+                text.append(line).append("\n");
             }
-            // System.out.println(text);
             br.close();
             return text;
         }catch(IOException e){
@@ -21,19 +29,23 @@ public class FileUtil {
             return null;
         }
     }
-
+    /**
+     * 文字コードを指定してファイルを読み込む関数
+     * @param filename 読み込むファイル名
+     * @param enc 文字コードの指定
+     * @return 各行の末尾に改行文字加えた文字列
+     */
     public static String readTextFile(String filename, String enc){
         try{
             String line;
-            String text = "";
             BufferedReader br =
             new BufferedReader(
                 new InputStreamReader(
                     new FileInputStream(filename),enc));
+        StringBuilder text = new StringBuilder();
         while((line = br.readLine()) != null){
-            text += line + "\n";
+           text.append(line).append("/n");
         }
-        // System.out.println(text);
         br.close();
         return text;
         }catch(IOException e){
@@ -41,7 +53,11 @@ public class FileUtil {
             return null;
         }
 }
-
+    /**
+     * ファイル名を書き出す関数
+     * @param data ファイルに書き出す内容
+     * @param filename 書き出すファイル名
+     */
     public static void writeTextFile(String data, String filename){ 
         try{
             PrintWriter pw =
@@ -57,7 +73,12 @@ public class FileUtil {
             e.printStackTrace();
         }
     }
-
+    /**
+     * 文字コードを指定してファイル名を書き出す関数
+     * @param data ファイルに書き出す内容
+     * @param filename 書き出すファイル名
+     * @param enc 文字コード
+     */
     public static void writeTextFile(String data, String filename, String enc){
         try{
             PrintWriter pw =
