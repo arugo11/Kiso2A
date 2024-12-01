562539e6021c8a0e662310ec06f320de1597def4
Argo11 <al23088@shibaura-it.ac.jp>
2024-12-01 15:43:52 +0900

    feat:kadai3

--- lesson8/BFS.java
+++ lesson8/BFS.java
@@ -6,7 +6,7 @@ public class BFS {
 
   public BFS(List<LinkedList<Integer>> adj, int s) { // 整数型の要素を持つリンクリストのリストadjと始点sを引数とする．
     int numV = adj.size(); // ネットワークの頂点数 
-    boolean done[] = new boolean[numV]; // 訪問したかのbooleanリスト(サイズは頂点数)
+    done = new boolean[numV]; // 訪問したかのbooleanリスト(サイズは頂点数)
 
     Q = new LinkedList<Integer>(); // Qのインスタンスを作成
 
@@ -14,20 +14,28 @@ public class BFS {
     // 以下コメントアウトの行を描きなさい．
 
     //頂点sは訪問した印をつける
+    done[s] = true;
     // 頂点sをキューに入れ初期化
+    Q.add(s);
     // while(キューQが空でない){
-    // キューQから先頭の要素を取得しuとする．その後，この要素をキューQから削除する．
-    System.out.print(u); // // キューから取り出した先頭要素uをプリント．
-    Iterator<Integer> itr = adj.get(u).iterator(); // // ノードuの隣接頂点のリストからIteratorのitrを作成．返り値の型は整数．
-  　// while( uの隣接要素がまだある? ) {
-    int v = itr.next(); // // 取得した隣接要素をvとする
-    // if(vがまだ訪問されていない){
-    System.out.print(">"); // // 次を指す矢印を出力
-    // vに訪問したという印をつける
-    // vをキューQに入れる
-    // if文を閉じる
-    // while文を閉じる
-    // while文を閉じる
+    while(!Q.isEmpty()) {
+        // キューQから先頭の要素を取得しuとする．その後，この要素をキューQから削除する．
+        int u = Q.poll();
+        System.out.print(u); // // キューから取り出した先頭要素uをプリント．
+        Iterator<Integer> itr = adj.get(u).iterator(); // // ノードuの隣接頂点のリストからIteratorのitrを作成．返り値の型は整数．
+        // while( uの隣接要素がまだある? ) {
+        while(itr.hasNext()) {
+            int v = itr.next(); // // 取得した隣接要素をvとする
+            // if(vがまだ訪問されていない){
+            if(!done[v]) {
+                System.out.print(">"); // // 次を指す矢印を出力
+                // vに訪問したという印をつける
+                done[v] = true;
+                // vをキューQに入れる
+                Q.add(v);
+            } // if文を閉じる
+        } // while文を閉じる
+    } // while文を閉じる
     
     //////////////////////////////////////////////////////
 
